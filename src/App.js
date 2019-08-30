e8c60a707bb4dc691d31883c5df6175fbd9464da
Thinh Mazeika <thinh.mazeika@gmail.com>
2019-08-30 16:33:39 -0500

    adding files for github pages

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
+import { SearchBox } from './components/search-box/search-box.component';
+
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [ ],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value});
+  };
+
+  render() {
+    const { monsters, searchField } = this.state; //const monsters = this.state.monsters ; const searchField = this.state.searchField;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">  
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='search monsters'
+         onSearchChange={this.onSearchChange}/> 
+        <CardList monsters={filteredMonsters} />
+      </div>    
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, onSearchChange }) => (
+  <input
+  className='search'
+  type='search'
+  placeholder={placeholder}
+  onChange={onSearchChange} 
+  />
+);
